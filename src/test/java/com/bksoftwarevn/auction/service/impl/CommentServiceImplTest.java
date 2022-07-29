7929506f11b4c19fa2930b8cb599c7dbcbf20f8b
cuongnm11 <cuongnm11@techcombank.com.vn>
2022-07-29 17:01:50 +0700

    commit code

--- src/test/java/com/bksoftwarevn/auction/service/impl/CommentServiceImplTest.java
+++ src/test/java/com/bksoftwarevn/auction/service/impl/CommentServiceImplTest.java
@@ -1,15 +1,12 @@
 package com.bksoftwarevn.auction.service.impl;
 
+import com.bksoftwarevn.auction.constant.AuctionStatus;
 import com.bksoftwarevn.auction.dto.PaginationDTO;
 import com.bksoftwarevn.auction.dto.SearchDTO;
 import com.bksoftwarevn.auction.mapper.CommentMapper;
 import com.bksoftwarevn.auction.mapper.LikeAuctionMapper;
-import com.bksoftwarevn.auction.model.CommentsResponse;
-import com.bksoftwarevn.auction.model.CommonResponse;
-import com.bksoftwarevn.auction.model.SearchCommentRequest;
-import com.bksoftwarevn.auction.persistence.entity.AuctionEntity;
-import com.bksoftwarevn.auction.persistence.entity.CommentEntity;
-import com.bksoftwarevn.auction.persistence.entity.UserEntity;
+import com.bksoftwarevn.auction.model.*;
+import com.bksoftwarevn.auction.persistence.entity.*;
 import com.bksoftwarevn.auction.persistence.repository.AuctionRepository;
 import com.bksoftwarevn.auction.persistence.repository.CommentRepository;
 import com.bksoftwarevn.auction.persistence.repository.LikeAuctionRepository;
@@ -19,6 +16,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -131,22 +130,373 @@ class CommentServiceImplTest {
 
     @Test
     void give_whenCreate() {
+        CreateCommentRequest createCommentRequest = mock(CreateCommentRequest.class);
 
+        when(createCommentRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(auctionRepository.findByIdAndStatus(MOCK_ID, AuctionStatus.STARTING.name())).thenReturn(Optional.empty());
+
+        CreateCommentResponse actualResult = commentService.create(MOCK_ID, createCommentRequest);
+
+        Assertions.assertNotNull(actualResult);
     }
 
     @Test
-    void update() {
+    void give1_whenCreate() {
+        CreateCommentRequest createCommentRequest = mock(CreateCommentRequest.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+
+        when(createCommentRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(auctionRepository.findByIdAndStatus(MOCK_ID, AuctionStatus.STARTING.name())).thenReturn(Optional.of(auctionEntity));
+
+        CreateCommentResponse actualResult = commentService.create(MOCK_ID, createCommentRequest);
+
+        Assertions.assertNotNull(actualResult);
     }
 
     @Test
-    void getAuctionsLiked() {
+    void give2_whenCreate() {
+        CreateCommentRequest createCommentRequest = mock(CreateCommentRequest.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+
+        when(createCommentRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(auctionRepository.findByIdAndStatus(MOCK_ID, AuctionStatus.STARTING.name())).thenReturn(Optional.of(auctionEntity));
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.empty());
+
+        CreateCommentResponse actualResult = commentService.create(MOCK_ID, createCommentRequest);
+
+        Assertions.assertNotNull(actualResult);
     }
 
     @Test
-    void totalLiked() {
+    void give3_whenCreate() {
+        CreateCommentRequest createCommentRequest = mock(CreateCommentRequest.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(createCommentRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(auctionRepository.findByIdAndStatus(MOCK_ID, AuctionStatus.STARTING.name())).thenReturn(Optional.of(auctionEntity));
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+
+        CreateCommentResponse actualResult = commentService.create(MOCK_ID, createCommentRequest);
+
+        Assertions.assertNotNull(actualResult);
     }
 
     @Test
-    void likeOrUnlike() {
+    void give4_whenCreate() {
+        CreateCommentRequest createCommentRequest = mock(CreateCommentRequest.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(createCommentRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(createCommentRequest.getParentId()).thenReturn(MOCK_ID);
+        when(repository.findById(MOCK_ID)).thenReturn(Optional.empty());
+        when(auctionRepository.findByIdAndStatus(MOCK_ID, AuctionStatus.STARTING.name())).thenReturn(Optional.of(auctionEntity));
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+
+        CreateCommentResponse actualResult = commentService.create(MOCK_ID, createCommentRequest);
+
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give5_whenCreate() {
+        CreateCommentRequest createCommentRequest = mock(CreateCommentRequest.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+        CommentEntity commentEntity = mock(CommentEntity.class);
+
+        when(createCommentRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(createCommentRequest.getParentId()).thenReturn(MOCK_ID);
+        when(repository.findById(MOCK_ID)).thenReturn(Optional.of(commentEntity));
+        when(repository.save(any(CommentEntity.class))).thenReturn(commentEntity);
+        when(auctionRepository.findByIdAndStatus(MOCK_ID, AuctionStatus.STARTING.name())).thenReturn(Optional.of(auctionEntity));
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+
+        CreateCommentResponse actualResult = commentService.create(MOCK_ID, createCommentRequest);
+
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give6_whenCreate() {
+        CreateCommentRequest createCommentRequest = mock(CreateCommentRequest.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+        CommentEntity commentEntity = mock(CommentEntity.class);
+
+        when(createCommentRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(createCommentRequest.getParentId()).thenReturn(MOCK_ID);
+        when(repository.findById(MOCK_ID)).thenReturn(Optional.of(commentEntity));
+        when(repository.save(any(CommentEntity.class))).thenReturn(null);
+        when(auctionRepository.findByIdAndStatus(MOCK_ID, AuctionStatus.STARTING.name())).thenReturn(Optional.of(auctionEntity));
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+
+        CreateCommentResponse actualResult = commentService.create(MOCK_ID, createCommentRequest);
+
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give1_whenUpdate() {
+        UpdateCommentRequest updateCommentRequest = mock(UpdateCommentRequest.class);
+
+        when(updateCommentRequest.getId()).thenReturn(MOCK_ID);
+        when(repository.findById(MOCK_ID)).thenReturn(Optional.empty());
+
+        CreateCommentResponse expectedResult = commentService.update(MOCK_ID, updateCommentRequest);
+
+        Assertions.assertNotNull(expectedResult);
+    }
+
+    @Test
+    void give2_whenUpdate() {
+        UpdateCommentRequest updateCommentRequest = mock(UpdateCommentRequest.class);
+        CommentEntity entity = mock(CommentEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(entity.getUser()).thenReturn(userEntity);
+        when(userEntity.getId()).thenReturn("123");
+        when(updateCommentRequest.getId()).thenReturn(MOCK_ID);
+        when(repository.findById(MOCK_ID)).thenReturn(Optional.of(entity));
+
+        CreateCommentResponse expectedResult = commentService.update(MOCK_ID, updateCommentRequest);
+
+        Assertions.assertNotNull(expectedResult);
+    }
+
+    @Test
+    void give3_whenUpdate() {
+        UpdateCommentRequest updateCommentRequest = mock(UpdateCommentRequest.class);
+        CommentEntity entity = mock(CommentEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(entity.getUser()).thenReturn(userEntity);
+        when(userEntity.getId()).thenReturn(MOCK_ID);
+        when(updateCommentRequest.getId()).thenReturn(MOCK_ID);
+        when(repository.save(any(CommentEntity.class))).thenReturn(null);
+        when(repository.findById(MOCK_ID)).thenReturn(Optional.of(entity));
+
+        CreateCommentResponse expectedResult = commentService.update(MOCK_ID, updateCommentRequest);
+
+        Assertions.assertNotNull(expectedResult);
+    }
+
+    @Test
+    void give4_whenUpdate() {
+        UpdateCommentRequest updateCommentRequest = mock(UpdateCommentRequest.class);
+        CommentEntity entity = mock(CommentEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(entity.getUser()).thenReturn(userEntity);
+        when(userEntity.getId()).thenReturn(MOCK_ID);
+        when(updateCommentRequest.getId()).thenReturn(MOCK_ID);
+        when(repository.save(any(CommentEntity.class))).thenReturn(entity);
+        when(repository.findById(MOCK_ID)).thenReturn(Optional.of(entity));
+
+        CreateCommentResponse expectedResult = commentService.update(MOCK_ID, updateCommentRequest);
+
+        Assertions.assertNotNull(expectedResult);
+    }
+
+    @Test
+    void give1_getAuctionsLiked() {
+        SearchAuctionLikedRequest searchAuctionLikedRequest = mock(SearchAuctionLikedRequest.class);
+
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.empty());
+
+        SearchAuctionsLikedResponse actualResponse = commentService.getAuctionsLiked(MOCK_ID, searchAuctionLikedRequest);
+        Assertions.assertNotNull(actualResponse);
+    }
+
+    @Test
+    void give2_getAuctionsLiked() {
+        SearchAuctionLikedRequest searchAuctionLikedRequest = mock(SearchAuctionLikedRequest.class);
+        UserEntity userEntity = mock(UserEntity.class);
+        PaginationDTO paginationDTO = mock(PaginationDTO.class);
+        List<LikeAuctionEntity> items = new ArrayList<>();
+        LikeAuctionEntity auctionEntity = new LikeAuctionEntity();
+        auctionEntity.setIsLiked(true);
+        items.add(auctionEntity);
+        AuctionLikedItem auctionLikedItem = mock(AuctionLikedItem.class);
+
+        MockedStatic<SearchDTO> mockedStatic = mockStatic(SearchDTO.class);
+        SearchDTO searchDTO = mock(SearchDTO.class);
+        SearchDTO.SearchDTOBuilder searchDTOBuilder = mock(SearchDTO.SearchDTOBuilder.class);
+        mockedStatic.when(() -> SearchDTO.builder()).thenReturn(searchDTOBuilder);
+        when(searchDTOBuilder.page(anyLong())).thenReturn(searchDTOBuilder);
+        when(searchDTOBuilder.size(anyLong())).thenReturn(searchDTOBuilder);
+        when(searchDTOBuilder.orders(anyList())).thenReturn(searchDTOBuilder);
+        when(searchDTOBuilder.conditions(anyList())).thenReturn(searchDTOBuilder);
+        when(searchDTOBuilder.build()).thenReturn(searchDTO);
+
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+        when(commonQueryService.search(LikeAuctionEntity.class, LikeAuctionEntity.class, searchDTO, null))
+                .thenReturn(paginationDTO);
+        when(paginationDTO.getItems()).thenReturn(items);
+        when(likeAuctionMapper.mappingAuctionToItem(any(LikeAuctionEntity.class))).thenReturn(auctionLikedItem);
+
+        SearchAuctionsLikedResponse actualResponse = commentService.getAuctionsLiked(MOCK_ID, searchAuctionLikedRequest);
+        Assertions.assertNotNull(actualResponse);
+        mockedStatic.close();
+    }
+
+    @Test
+    void give1_totalLiked() {
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.empty());
+
+        TotalLikeResponse actualResponse = commentService.totalLiked(MOCK_ID);
+        Assertions.assertNotNull(actualResponse);
+    }
+
+    @Test
+    void give2_totalLiked() {
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+
+        TotalLikeResponse actualResponse = commentService.totalLiked(MOCK_ID);
+        Assertions.assertNotNull(actualResponse);
+    }
+
+    @Test
+    void give1_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.empty());
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give2_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.empty());
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give3_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give4_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+        when(auctionEntity.getStatus()).thenReturn(AuctionStatus.REJECTED.name());
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give5_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+        when(auctionEntity.getStatus()).thenReturn(AuctionStatus.PENDING.name());
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give6_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+        when(auctionEntity.getStatus()).thenReturn(AuctionStatus.WAITING.name());
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.empty());
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give7_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+        when(auctionEntity.getStatus()).thenReturn(AuctionStatus.WAITING.name());
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+        when(likeAuctionRepository.save(any(LikeAuctionEntity.class))).thenReturn(likeAuctionEntity);
+        when(likeAuctionEntity.getIsLiked()).thenReturn(false);
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give8_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+        when(auctionEntity.getStatus()).thenReturn(AuctionStatus.WAITING.name());
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+        when(likeAuctionRepository.save(any(LikeAuctionEntity.class))).thenReturn(null);
+        when(likeAuctionEntity.getIsLiked()).thenReturn(null);
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
+    }
+
+    @Test
+    void give9_whenLikeOrUnlike() {
+        CreateLikeRequest createLikeRequest = mock(CreateLikeRequest.class);
+        LikeAuctionEntity likeAuctionEntity = mock(LikeAuctionEntity.class);
+        AuctionEntity auctionEntity = mock(AuctionEntity.class);
+        UserEntity userEntity = mock(UserEntity.class);
+
+        when(createLikeRequest.getAuctionId()).thenReturn(MOCK_ID);
+        when(likeAuctionRepository.findById(any(LikeAuctionEntityId.class))).thenReturn(Optional.of(likeAuctionEntity));
+        when(auctionRepository.findById(MOCK_ID)).thenReturn(Optional.of(auctionEntity));
+        when(auctionEntity.getStatus()).thenReturn(AuctionStatus.WAITING.name());
+        when(userRepository.findById(MOCK_ID)).thenReturn(Optional.of(userEntity));
+        when(likeAuctionRepository.save(any(LikeAuctionEntity.class))).thenReturn(null);
+        when(likeAuctionEntity.getIsLiked()).thenReturn(true);
+
+        CreateLikeResponse actualResult = commentService.likeOrUnlike(MOCK_ID, createLikeRequest);
+        Assertions.assertNotNull(actualResult);
     }
 }
